287c351c9fdae1827336bb8162467e9dd1362c51
Md Eusuf Uddin <eusuf.uddin@v2.ltd>
2024-11-15 12:31:04 +0600

    disable when slider loading

--- lib/main.dart
+++ lib/main.dart
@@ -31,57 +31,56 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SizedBox(
-            height: 62,
-            child: SlideAction(
-              key: _slideActionKey, // Assign the key to the SlideAction
-              text: 'Slide to start Market Visit',
-              textStyle: const TextStyle(color: Colors.white, fontSize: 16),
-              innerColor: Colors.yellow.withOpacity(0.4),
-              elevation: 0,
-              enabled: !isLoading,
-              outerColor: Colors.blue.withOpacity(0.8),
-              sliderButtonIconPadding: 4,
-              sliderButtonIcon: Center(
-                child: isLoading
-                    ? Transform.scale(
-                        scale: 0.9,
-                        child: const CircularProgressIndicator(
-                          strokeWidth: 3.0,
-                          color: Colors.pink,
-                        ),
-                      )
-                    : const Icon(
-                        Icons.arrow_right_alt_outlined,
-                        color: Colors.white,
-                        size: 40.0,
-                        semanticLabel: 'Text to announce in accessibility modes',
+        child: SizedBox(
+          width: MediaQuery.sizeOf(context).width * 0.76,
+          child: SlideAction(
+            key: _slideActionKey, // Assign the key to the SlideAction
+            text: 'Start Market Visit',
+            textStyle: const TextStyle(color: Colors.white, fontSize: 16),
+            innerColor: Colors.yellow.withOpacity(0.4),
+            elevation: 0,
+            height: 58,
+            enabled: !isLoading,
+            outerColor: Colors.blue.withOpacity(0.8),
+            sliderButtonIconPadding: 4,
+            sliderButtonIcon: Center(
+              child: isLoading
+                  ? Transform.scale(
+                      scale: 0.8,
+                      child: const CircularProgressIndicator(
+                        strokeWidth: 3.0,
+                        color: Colors.pink,
                       ),
-              ),
-              onSubmit: () async {
-                // Check again to ensure that the slide action only triggers once.
-                if (isLoading) return;
-
-                setState(() {
-                  isLoading = true; // Show loading spinner
-                });
-
-                print('Sliding action initiated...');
-
-                // Reset the slider position immediately after triggering the action
-
-                // Simulate a delay (e.g., for an API call or other asynchronous task)
-                await Future.delayed(const Duration(seconds: 5), () {
-                  print('Market visit started!');
-                  setState(() {
-                    isLoading = false; // Hide loading spinner
-                  });
-                });
-                _slideActionKey.currentState?.reset();
-              },
+                    )
+                  : const Icon(
+                      Icons.arrow_right_alt_outlined,
+                      color: Colors.white,
+                      size: 40.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    ),
             ),
+            onSubmit: () async {
+              // Check again to ensure that the slide action only triggers once.
+              if (isLoading) return;
+
+              setState(() {
+                isLoading = true; // Show loading spinner
+              });
+
+              print('Sliding action initiated...');
+
+              // Reset the slider position immediately after triggering the action
+
+              // Simulate a delay (e.g., for an API call or other asynchronous task)
+              await Future.delayed(const Duration(seconds: 5), () {
+                print('Market visit started!');
+                setState(() {
+                  isLoading = false; // Hide loading spinner
+                });
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Slider task Done")));
+              });
+              _slideActionKey.currentState?.reset();
+            },
           ),
         ),
       ),
